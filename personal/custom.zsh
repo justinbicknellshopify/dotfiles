3822a9811739e8d9e15302e0821e466e4835e3ed
Hisham Kalban <hishamkalban@Hishams-MacBook-Pro.local>
2022-03-08 11:14:55 -0600

    added g alias

--- personal/custom.zsh
+++ personal/custom.zsh
@@ -2,3 +2,5 @@
 # This file is loaded after everything else, but before Antigen is applied and ~/extra.zsh sourced.
 # Put anything here that you want to exist on all your environment, and to have the highest priority
 # over any other customization.
+
+alias g='git'
