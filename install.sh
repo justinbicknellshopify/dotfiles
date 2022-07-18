f48094978e035939792ddabf7aa27c555fe8b844
Hisham Kalban <hishamkalban@Hishams-MacBook-Pro.local>
2022-07-18 17:17:49 -0500

    Update template

--- install.sh
+++ install.sh
@@ -9,6 +9,19 @@ colors
 # and that you don't want to follow you across environments.
 touch ~/extra.zsh
 
+DOTFILES_DIRECTORY_NAME=$([ $SPIN ] && echo "shopify-dotfiles" || echo "dotfiles")
+ZSH_HOST_OS=$(uname | awk '{print tolower($0)}')
+
+case $ZSH_HOST_OS in
+  darwin*)
+
+  BREW_EXECUTABLE=/opt/homebrew/bin/brew
+
+  $BREW_EXECUTABLE shellenv > $HOME/.dotfile_brew_setup
+  $BREW_EXECUTABLE install coreutils
+;;
+esac
+
 # Install the antigen plugin/theme manager if it's not already installed.
 if [[ ! -d $HOME/antigen ]]; then
 	echo -e "Antigen not found, installing..."
@@ -31,12 +44,12 @@ fi
 
 # Link in the custom gitconfig. This has to happen after we rename to .gitconfig.local, otherwise we clobber the
 # spin generated user config.
-ln -vsfn ~/dotfiles/core/configs/.gitconfig ~/.gitconfig
-ln -vsfn ~/dotfiles/core/configs/.gitignore_global ~/.gitignore_global
+ln -vsfn ~/$DOTFILES_DIRECTORY_NAME/core/configs/.gitconfig ~/.gitconfig
+ln -vsfn ~/$DOTFILES_DIRECTORY_NAME/core/configs/.gitignore_global ~/.gitignore_global
 
 # Symlink this repo's .zshrc to ~/.zshrc. Using a symlink ensures that when the repo is
 # updated, the terminal will pick up the new version on reload without having to run
 # install again. This will overwrite any existing .zshrc.
-ln -vsfn ~/dotfiles/.zshrc ~/.zshrc
+ln -vsfn ~/$DOTFILES_DIRECTORY_NAME/.zshrc ~/.zshrc
 
-source ~/dotfiles/personal/install.sh
+source ~/$DOTFILES_DIRECTORY_NAME/personal/install.sh
